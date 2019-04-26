b32ee12add7f2cf056244c1a86d3230f63f14a47
Julian Cuni <juliancuni@gmail.com>
2019-04-26 19:38:43 +0200

    Add project files.

---
+++ Middleware_TimeAtt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middleware_TimeAtt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
