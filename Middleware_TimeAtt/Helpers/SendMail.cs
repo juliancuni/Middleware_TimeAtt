5844a577f58d6fe8e1e3ba6495385617ddaa0d8b
Julian Cuni <juliancuni@gmail.com>
2019-04-26 23:11:09 +0200

    Send Mail

---
+++ Middleware_TimeAtt/Helpers/SendMail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Middleware_TimeAtt
+{
+    class SendMail
+    {
+        public static void Send(string subject, string body)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient(ConfReader.Read("smtpserver"));
+
+                mail.From = new MailAddress(ConfReader.Read("senderemail"));
+                mail.To.Add(ConfReader.Read("contactemail"));
+                mail.Subject = subject;
+                mail.Body = body;
+
+                SmtpServer.Port = Int32.Parse(ConfReader.Read("smtpserverport"));
+                SmtpServer.Credentials = new System.Net.NetworkCredential(ConfReader.Read("senderemail"), ConfReader.Read("senderemailpass"));
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                Logger.WriteLog("INFO: mail Send to " + ConfReader.Read("contactemail"));
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog("ERROR: mail Send failed" + ex.Message.ToString().Trim());
+            }
+        }
+    }
+}
